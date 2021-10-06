9a82c329199e99cb94e9dd5786e386465072b7e2
Pedro <pbbdasilva@gmail.com>
2021-10-06 00:48:04 -0300

    Initial commit adding board and players

---
+++ board.py
@@ -0,0 +1,49 @@
+from enums import Squares as sq
+from enums import Directions as dir
+from player import Player
+import math
+
+class Board:
+    def __init__( self, size, addr1, addr2 ):
+        self.players = { sq.P1 : 0, sq.P2 : 1 }
+        self.p = [  Player(sq.P1, addr1, math.floor(size/2), math.floor(size/4)),
+                    Player(sq.P2, addr2, math.floor(size/2), math.floor(3*size/4)) ]
+
+        self.size = size
+        self.board = self.init_board( size )
+
+        self.directions = { dir.RIGHT : 0, dir.UP : 1, dir.LEFT : 2, dir.DOWN : 3 }
+        self.dx = [ 1, 0, -1,  0 ]
+        self.dy = [ 0, 1,  0, -1 ]
+
+    def init_board( self, size ):
+        board = [ [ sq.EMPTY for i in range( size ) ] for j in range( size ) ]
+        board[ math.floor( size/2 ) ][ math.floor( size/4 ) ] = sq.P1
+        board[ math.floor( size/2 ) ][ math.floor( 3*size/4 ) ] = sq.P2
+
+        return board
+
+    def move( self, player, nxt_direction ):
+        curr_x = self.p[ player ].x
+        curr_y = self.p[ player ].y
+
+        next_x = curr_x + self.dx[ self.directions[ nxt_direction ] ]
+        next_y = curr_y + self.dy[ self.directions[ nxt_direction ] ]
+
+        if ( next_x < 0 or next_x > self.size ):
+            return -1
+
+        if ( next_y < 0 or next_y > self.size ):
+            return -1
+
+        self.board[ next_x ][ next_y ] = player
+
+    def show( self ):
+        pass
+
+def main():
+    b = Board( 10 )
+    b.show()
+
+if ( __name__ == '__main__' ):
+    main()
---
+++ player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, addr, init_x, init_y, color):
+        self.color = color
+        self.addr = addr
+        self.x = init_x
+        self.y = init_y
