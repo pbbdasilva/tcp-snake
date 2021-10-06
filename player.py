7bdd57afff16cec5d68f97c6ed4fa31b4c0850a0
Pedro <pbbdasilva@gmail.com>
2021-10-06 17:53:47 -0300

    Added Player.update method to store current location of each player

--- player.py
+++ player.py
@@ -1,6 +1,10 @@
 class Player:
-    def __init__(self, addr, init_x, init_y, color):
+    def __init__( self, addr, init_x, init_y, color ):
         self.color = color
         self.addr = addr
         self.x = init_x
         self.y = init_y
+
+    def update( self, nxt_x, nxt_y ):
+        self.x = nxt_x
+        self.y = nxt_y
