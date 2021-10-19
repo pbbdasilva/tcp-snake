b26bc624813f1d1fcdc00709f6c4953925feb4dd
bereznik <bernardreznik@gmail.com>
2021-10-19 11:49:10 -0300

    included render method to initially show the game window

--- game_client.py
+++ game_client.py
@@ -1,5 +1,6 @@
 from board import Board
-from game_client import Protocol_client
+from client_protocol import Protocol_client
+from curses import textpad
 from enums import Squares as sq
 import curses
 import socket
@@ -31,7 +32,7 @@ class Game:
 
         self.set_game( screen )
         screen.clear()
-        self.render( screen )
+        self.render( screen, board )
 
         key_pressed = 0
         server_input = Protocol_client()
@@ -39,7 +40,39 @@ class Game:
         while( not server_input.end_game ):
             acc = 0.0
 
-    def render( self, screen ):
+    def render( self, screen, board ):
+        curses.curs_set(0)
+
+        sh, sw = screen.getmaxyx()
+        
+        N = len(board)
+        
+        box = [[3,3],[sh-3,sw-3]]
+
+        textpad.rectangle(screen, box[0][0],box[0][1],box[1][0], box[1][1])
+        
+        y_start,x_start  = sh//2-N//2,sw//2-(2*N-1)//2
+        y,x = y_start,x_start
+        
+        for line in board:
+            
+            for element in line:
+                if element == sq.EMPTY:
+                    screen.addch(y,x,element.value, curses.color_pair(1))
+                elif element == sq.P1:
+                    screen.addch(y,x,element.value, curses.color_pair(2))
+                elif element == sq.P2:
+                    screen.addch(y,x,element.value, curses.color_pair(3)) 
+                
+                x = x+2
+            
+            x = x_start
+            y = y+1
+        
+        screen.refresh()
+        screen.getch()
+    
+    def update_screen(self,screen):
         pass
 
     def run( self ):
@@ -52,3 +85,4 @@ class Game:
     def start( self ):
         self.set_game()
         curses.wrapper( self.game_window )
+
